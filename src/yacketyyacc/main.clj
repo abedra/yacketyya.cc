840d37e87971ba521d9d01bb221d5c048c4208a2
Aaron Bedra <aaron@aaronbedra.com>
2009-10-28 11:32:24 -0400

    if the requested url already exists, don't create another shortened url

--- src/yacketyyacc/main.clj
+++ src/yacketyyacc/main.clj
@@ -2,19 +2,6 @@
   (:use compojure)
   (:require [yacketyyacc.models.url :as url]))
 
-(def chars (map char (concat (range 48 58) (range 66 92) (range 97 123))))
-
-(defn random-char
-  "Generates a random character"
-  []
-  (nth chars
-       (.nextInt (java.util.Random.) (count chars))))
-
-(defn random-string
-  "Generates a random string of length n"
-  [length]
-  (apply str (take length (repeatedly random-char))))
-
 (defn index []
   (html (form-to [:post "/"]
                  [:label {:for "url"} "URL: "]
@@ -22,9 +9,7 @@
                  (submit-button "Shorten"))))
 
 (defn post-link [params]
-  (let [shortened (random-string 5)]
-    (url/create {:original_url (params :url) :shortened_url shortened})
-    (str "http://yacketyya.cc/" shortened)))
+  (str "Your shortened url is: http://yacketyya.cc/" ((url/find-or-create (params :url)) :shortened_url)))
 
 (defn lookup-url [params]
   (redirect-to ((url/find-by-slug (params :url)) :original_url)))
--- src/yacketyyacc/models/url.clj
+++ src/yacketyyacc/models/url.clj
@@ -9,5 +9,24 @@
 
 (clj-record.core/init-model)
 
+(def chars (map char (concat (range 48 58) (range 66 92) (range 97 123))))
+
+(defn random-char
+  "Generates a random character"
+  []
+  (nth chars
+       (.nextInt (java.util.Random.) (count chars))))
+
+(defn random-string
+  "Generates a random string of length n"
+  [length]
+  (apply str (take length (repeatedly random-char))))
+
 (defn find-by-slug [slug]
-  (first (find-by-sql ["select * from urls where shortened_url = ?", slug])))
+  (first (find-by-sql ["select * from urls where shortened_url = ? limit 1", slug])))
+
+(defn find-or-create [original_url]
+  (let [lookup (first (find-by-sql ["select * from urls where original_url = ? limit 1", original_url]))]
+    (if lookup
+      lookup
+      (create {:original_url original_url :shortened_url (random-string 5)}))))
