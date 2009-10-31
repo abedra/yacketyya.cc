c88cfbbea07ef8c8f26130c133c78e8e37d42cd5
Aaron Bedra <aaron@aaronbedra.com>
2009-10-31 12:34:28 -0400

    render not found when shortened url doesn't exist.  cleanup

--- src/yacketyyacc/main.clj
+++ src/yacketyyacc/main.clj
@@ -11,23 +11,18 @@
 
 (defn lookup-url [params]
   (let [url (url/find-by-slug (params :url))]
-    (url/update {:id (url :id) :clicks (+ (url :clicks) 1)})
-    (redirect-to (url :original_url))))
+    (if (nil? url)
+      (page-not-found)
+      (do
+        (url/update {:id (url :id) :clicks (+ (url :clicks) 1)})
+        (redirect-to (url :original_url))))))
 
 (defn about []
   (render-template "about" {}))
-
-(defn instructions []
-  (render-template "instructions" {}))
-
-(defn contact []
-  (render-template "contact" {}))
 
 (defroutes yacketyyacc
   (GET  "/"  (index))
   (POST "/"  (post-link params))
   (GET  "/about" (about))
-  (GET  "/instructions" (instructions))
-  (GET  "/contact" (contact))
   (GET  "/:url" (lookup-url params))
   (GET  "/assets/*" (or (serve-file (params :*)) :next)))
